702a7e64c0cf2da8a7e5c3527c7927bcba9d3fdf
marty-crane <kyam.lh@googlemail.com>
2018-03-21 15:55:41 +0000

    first PF item autofills

--- portfolioLogic.php
+++ portfolioLogic.php
@@ -3,31 +3,34 @@ $db = new PDO('mysql:host=127.0.0.1; dbname=portfolioKyam', 'root');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 function createFirstPfItem($db) {
-    $query=$db->prepare("SELECT `title`,`description`,`imgRef`,`projURL`,`github` FROM `portfolioItems` WHERE `deleted` !=1 LIMIT 1");
+    $query=$db->prepare("SELECT `title`,`description`,`imgRef`,`github`,`projURL`,`images`.`url`,`images`.`altText` 
+                         FROM `portfolioItems` 
+                         LEFT JOIN `images`
+                         ON `portfolioItems`.`imgRef`
+                         =`images`.`id`
+                         WHERE `portfolioItems`.`deleted` !=1 LIMIT 1;");
     $query->execute();
-    $query->fetchall();
-
-    return "<article class='primaryPfItem''>
+    $result= $query->fetchAll();
+    return "<article class='primaryPfItem'>
 				<section class='itemPic'>
-					<img src='assets/pilot.png' alt='image of pilot website'/>
+					<img src=" . $result[0]['url'] . " alt=" . $result[0]['altText'] . "/>
 				</section>
 				<section class='itemText'>
 					<h3>
-						<a href='#'>Pilot store</a>
+						<a href='" . $result[0]['projURL'] . "'>" . $result[0]['title'] . "</a>
 					</h3>
-					<p>Our first responsive web-page build has us mimicking the PilotShop website.
-    The challenges of this were not only mimicking the site from its initial appearance,
-						figuring our where the break-points needed to be and copying it's responses
-						proved to be challenging.
+					<p>" . $result[0]['description'] . "
 					</p>
 				</section>
 			</article>";
 }
 
+$query=$db->prepare("SELECT `title`,`description`,`imgRef`,`github`,`images`.`url`,`images`.`altText` 
+                         FROM `portfolioItems` 
+                         LEFT JOIN `images`
+                         ON `portfolioItems`.`imgRef`
+                         =`images`.`id`
+                         WHERE `portfolioItems`.`deleted` !=1 LIMIT 1;");
 
-
-$query=$db->prepare("SELECT `title`,`description`,`imgRef`,`projURL`,`github` FROM `portfolioItems` WHERE `deleted` !=1 && `title` ='Pilot Store' LIMIT 1");
 $query->execute();
-$results=$query->fetchall();
-
-var_dump($results);
+$result= $query->fetchAll();
